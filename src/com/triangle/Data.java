531109675d36736bf602effb7caa3d1abe4a6aaf
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-05 13:34:24 +0300

    add new features

--- src/com/triangle/Data.java
+++ src/com/triangle/Data.java
@@ -1,15 +1,36 @@
 package com.triangle;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Data {
-    String trianleInfo;
+    String triangleInfo;
     String[] triangleArr;
+    ArrayList<Triangle> triangles = new ArrayList<>();
+    String answer;
+    int sideA, sideB, sideC;
+    String triangleName;
+    Triangle t = new Triangle(triangleName, sideA, sideB, sideC);
+
 
     public void inputData() {
         Scanner s = new Scanner(System.in);
-        System.out.println("Please enter name and length for each side of triangle : ");
-        trianleInfo = s.nextLine();
-        triangleArr = trianleInfo.split(", ");
+        do {
+            System.out.println("Please enter name and length for each side of triangle : ");
+            triangleInfo = s.nextLine();
+            triangleArr = triangleInfo.split(", ");
+
+            triangleName = triangleArr[0];
+            sideA = parseInt(triangleArr[1]);
+            sideB = parseInt(triangleArr[2]);
+            sideC = parseInt(triangleArr[3]);
+
+            triangles.add(new Triangle (triangleName, sideA, sideB, sideC));
+            System.out.println("Would you like to continue (Y/N): ");
+            answer = s.nextLine();
+        }
+        while ((answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes")));
     }
 }
--- src/com/triangle/Triangle.java
+++ src/com/triangle/Triangle.java
@@ -1,18 +1,18 @@
 package com.triangle;
 
-import java.util.ArrayList;
-
 import static java.lang.Math.sqrt;
 
 public class Triangle {
-    String triangleName;
-    int sideA, sideB, sideC;
+    private String triangleName;
+    private int sideA;
+    private int sideB;
+    private int sideC;
 
     public Triangle(String triangleName, int sideA, int sideB, int sideC) {
-        this.triangleName = triangleName;
-        this.sideA = sideA;
-        this.sideB = sideB;
-        this.sideC = sideC;
+        this.setTriangleName(triangleName);
+        this.setSideA(sideA);
+        this.setSideB(sideB);
+        this.setSideC(sideC);
     }
 
     public String getTriangleName() {
@@ -31,11 +31,26 @@ public class Triangle {
         return sideC;
     }
 
-    ArrayList<Triangle> triangles = new ArrayList<>();
 
-    public float area(int sideA, int sideB, int sideC) {
+    public float area() {
         float perimetr;
-        perimetr = (sideA + sideB + sideC) / 2;
-        return (float) sqrt((perimetr * (perimetr - sideA) * (perimetr - sideB) * (perimetr - sideC)));
+        perimetr = (getSideA() + getSideB() + getSideC()) / 2;
+        return (float) sqrt((perimetr * (perimetr - getSideA()) * (perimetr - getSideB()) * (perimetr - getSideC())));
+    }
+
+    public void setTriangleName(String triangleName) {
+        this.triangleName = triangleName;
+    }
+
+    public void setSideA(int sideA) {
+        this.sideA = sideA;
+    }
+
+    public void setSideB(int sideB) {
+        this.sideB = sideB;
+    }
+
+    public void setSideC(int sideC) {
+        this.sideC = sideC;
     }
 }
